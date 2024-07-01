328ec0fe790aa0dfcea5b27f77f01168b939e010
Arpan Sardar <ankur.sardar18@gmail.com>
2024-07-01 08:13:58 +0530

    intermediate commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "@emotion/react";
-import theme from "./theme";
-import store from "./store.ts";
-import { Provider } from "react-redux";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<ThemeProvider theme={theme}>
-				<App />
-			</ThemeProvider>
-		</Provider>
+		<App />
 	</React.StrictMode>
 );
