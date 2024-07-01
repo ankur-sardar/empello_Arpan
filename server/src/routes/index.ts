9d7d8732e46acf412ca9a6c013d74226b078c6b5
Arpan Sardar <ankur.sardar18@gmail.com>
2024-07-02 02:02:17 +0530

    unused code removed

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import UserRoutes from "./userRoutes";
 import QuizRoutes from "./quizRoutes";
 const router = Router();
 
 // Routes
-router.use("/user", UserRoutes);
 router.use("/quiz", QuizRoutes);
 
 // Export the base-router
--- server/src/routes/quizRoutes.ts
+++ server/src/routes/quizRoutes.ts
@@ -1,20 +1,9 @@
 import express from "express";
 import authorize from "../middleware/authorize";
-import {
-	getCategories,
-	getQuizLevels,
-	createNewQuizGame,
-	submitCompletedGameData,
-	getGameDataById,
-	getPlayedQuizGames,
-} from "../controller/quizController";
+import { getCategories, createNewQuizGame } from "../controller/quizController";
 const router = express.Router();
 
 router.get("/categories", authorize, getCategories);
-router.get("/quizlevels", authorize, getQuizLevels);
 router.post("/createnewgame", authorize, createNewQuizGame);
-router.post("/submitgamedata", authorize, submitCompletedGameData);
-router.get("/getgamedatabyid", authorize, getGameDataById);
-router.get("/getplayedgames", authorize, getPlayedQuizGames);
 
 export default router;
