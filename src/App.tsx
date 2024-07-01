e0e977fc88f78addc8f92f7766bac717e2989210
Arpan Sardar <ankur.sardar18@gmail.com>
2024-07-01 13:38:16 +0530

    intermediate commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home.tsx";
-import ProgressLevel from "./pages/progressLevel.tsx";
-import QuizPlayground from "./pages/quizPlayground.tsx";
 import Layout from "./components/layout.tsx";
-import Play from "./components/quizPlayground/play.tsx";
-import Profile from "./pages/profile.tsx";
-import Leaderboard from "./pages/leaderboard.tsx";
 import "./App.css";
 
 const App = () => {
@@ -14,12 +9,6 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Home />} />
-					<Route path="/progresslevel" element={<ProgressLevel />} />
-					<Route path="/playground" element={<QuizPlayground />}>
-						<Route path="/playground/:quizId" element={<Play />} />
-					</Route>
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/leaderboard" element={<Leaderboard />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
